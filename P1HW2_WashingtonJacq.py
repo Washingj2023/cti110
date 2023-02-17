abeb22d6c6469d0d307d4eba5b443e9b4e7be059
Washingj2023 <124739685+Washingj2023@users.noreply.github.com>
2023-02-17 15:17:17 -0500

    Add files via upload

---
+++ P1HW2_WashingtonJacq.py
@@ -0,0 +1,27 @@
+print('Class travel Project')
+print('2/16/2023')
+print('CTI-110 P1HW2 - Travel Expense')
+print('Jacqueline Washington')
+
+
+budget=input('What is your budget?')
+print("My budget is $"+ budget)
+
+destination=input('What is your destination?')
+print("My destination is "+ destination)
+
+gas=input('What amount of money is for gas?')
+print("My gas amount is $ "+gas )
+
+accommodation=input('What amount of money is for accommodation?')
+print("My accommodation amount to spend is $ "+ accommodation)
+
+food=input('What amont of money is for food?')
+print("My food amount is $"+ food)
+
+
+
+
+
+
+
