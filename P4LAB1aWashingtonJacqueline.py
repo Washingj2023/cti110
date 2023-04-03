b7ddc50340e8ef8943542309bce77c1410880f02
Washingj2023 <124739685+Washingj2023@users.noreply.github.com>
2023-04-03 14:53:19 -0400

    Add files via upload

---
+++ P4LAB1aWashingtonJacqueline.py
@@ -0,0 +1,23 @@
+import turtle
+
+jackie = turtle.Turtle()
+jackie.color('blue')
+jackie = turtle.Turtle()
+jackie.forward(150)
+jackie.left(90)
+jackie.forward(150)
+jackie.left(90)
+jackie.forward(150)
+jackie.left(90)
+jackie.forward(150)
+jackie.left(90)
+jackie.forward(150)
+
+jackie.forward(100)  
+jackie.left(120)
+jackie.forward(100)
+ 
+jackie.left(120)
+jackie.forward(100)
+ 
+jackie.done()
